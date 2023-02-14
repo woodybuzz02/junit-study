1b88c0a6aab5bdd6564ba4adaea3789e356c46b3
yewon <ajdanddl6321@gmail.com>
2023-02-15 01:35:47 +0900

    initial setting

---
+++ src/test/java/com/example/junitproject/JunitProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.junitproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JunitProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
