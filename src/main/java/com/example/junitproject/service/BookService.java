1a7c7e3e3d7bb1a0e4af9a51a1415c8f72bb2166
yewon <ajdanddl6321@gmail.com>
2023-02-16 21:12:41 +0900

    책한건보기, 책삭제, 책수정 서비스 완료

--- src/main/java/com/example/junitproject/service/BookService.java
+++ src/main/java/com/example/junitproject/service/BookService.java
@@ -36,8 +36,30 @@ public class BookService {
     }
 
     // 3. 책한건보기
+    public BookRespDto 책한건보기(Long id){
+        Optional<Book> bookOP = bookRepository.findById(id);
+        if(bookOP.isPresent()){
+            return new BookRespDto().toDto(bookOP.get());
+        }else{
+            throw new RuntimeException("해당 아이디를 찾을 수 없습니다.");
+        }
 
+    }
     // 4. 책삭제
+    @Transactional(rollbackFor = RuntimeException.class)
+    public void 책삭제하기(Long id){
+        bookRepository.deleteById(id);
+    }
 
     // 5. 책수정
+    @Transactional(rollbackFor = RuntimeException.class)
+    public void 책수정하기(Long id, BookSaveReqDto dto){
+        Optional<Book> bookOP = bookRepository.findById(id);
+        if(bookOP.isPresent()){
+            Book bookPs = bookOP.get();
+            bookPs.update(dto.getTitle(), dto.getAuthor());
+        }else{
+            throw new RuntimeException("해당 아이디를 찾을 수 없습니다.");
+        }
+    }// 메서드 종료시에 더티체킹(flush)으로 update 된다.
 }
--- src/main/java/com/example/junitproject/web/dto/BookSaveReqDto.java
+++ src/main/java/com/example/junitproject/web/dto/BookSaveReqDto.java
@@ -1,8 +1,10 @@
 package com.example.junitproject.web.dto;
 
 import com.example.junitproject.domain.Book;
+import lombok.Getter;
 import lombok.Setter;
 
+@Getter
 @Setter // Controller에서 Setter가 호출되면서 Dto에 값이 채워짐.
 public class BookSaveReqDto {
 
