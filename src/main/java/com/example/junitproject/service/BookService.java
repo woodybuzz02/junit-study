37ce7b9a3b811b1a86e9d656e9bdf6b829ad078c
yewon <ajdanddl6321@gmail.com>
2023-02-17 00:24:28 +0900

    서비스 레이어 테스트 책목록보기, 책한건보기, 책수정하기 완료, 책삭제는 생략

--- src/main/java/com/example/junitproject/service/BookService.java
+++ src/main/java/com/example/junitproject/service/BookService.java
@@ -12,6 +12,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -60,11 +61,12 @@ public class BookService {
 
     // 5. 책수정
     @Transactional(rollbackFor = RuntimeException.class)
-    public void 책수정하기(Long id, BookSaveReqDto dto){
+    public BookRespDto 책수정하기(Long id, BookSaveReqDto dto){
         Optional<Book> bookOP = bookRepository.findById(id);
         if(bookOP.isPresent()){
             Book bookPs = bookOP.get();
             bookPs.update(dto.getTitle(), dto.getAuthor());
+            return new BookRespDto().toDto(bookPs);
         }else{
             throw new RuntimeException("해당 아이디를 찾을 수 없습니다.");
         }
--- src/test/java/com/example/junitproject/service/BookServiceTest.java
+++ src/test/java/com/example/junitproject/service/BookServiceTest.java
@@ -1,6 +1,8 @@
 package com.example.junitproject.service;
 
 import static org.assertj.core.api.Assertions.*;
+
+import com.example.junitproject.domain.Book;
 import com.example.junitproject.domain.BookRepository;
 import com.example.junitproject.util.MailSender;
 import com.example.junitproject.util.MailSenderStub;
@@ -13,6 +15,11 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
@@ -50,5 +57,71 @@ public class BookServiceTest {
         // assertEquals(dto.getAuthor(), bookRespDto.getAuthor());
         assertThat(dto.getTitle()).isEqualTo(bookRespDto.getTitle());
         assertThat(dto.getAuthor()).isEqualTo(bookRespDto.getAuthor());
+    }
+
+    @Test
+    public void 책목록보기_테스트(){
+        // given // 들어가야되는 값이 없으니 given은 없다.
+
+        // stub (행동정의)
+        List<Book> books = new ArrayList<>();
+        books.add(new Book(1l, "junit강의", "메타"));
+        books.add(new Book(2l, "스프링강의", "겟인데어"));
+        when(bookRepository.findAll()).thenReturn(books);
+
+        // when
+        List<BookRespDto> dtos = bookService.책목록보기();
+
+        // then
+        assertThat(dtos.get(0).getTitle()).isEqualTo("junit강의");
+        assertThat(dtos.get(0).getAuthor()).isEqualTo("메타");
+        assertThat(dtos.get(1).getTitle()).isEqualTo("스프링강의");
+        assertThat(dtos.get(1).getAuthor()).isEqualTo("겟인데어");
+    }
+
+    @Test
+    public void 책한건보기_테스트(){
+        // given
+
+        Long id = 1l;
+
+        // stub (행동정의)
+
+        Book book = new Book(1l,"junit강의","메타코딩");
+        Optional<Book> bookOP = Optional.of(book);
+        when(bookRepository.findById(id)).thenReturn(bookOP);
+
+        // when
+        BookRespDto dto = bookService.책한건보기(id);
+
+        // then
+        assertThat(dto.getTitle()).isEqualTo("junit강의");
+        assertThat(dto.getAuthor()).isEqualTo("메타코딩");
+
+    }
+
+
+    @Test
+    public void 책수정_테스트(){
+        // given
+
+        Long id = 1l;
+        BookSaveReqDto bookSaveReqDto = new BookSaveReqDto();
+        bookSaveReqDto.setTitle("스프링강의");
+        bookSaveReqDto.setAuthor("겟인데어");
+
+        // stub (행동정의)
+        Book book = new Book(1l,"junit강의","메타코딩");
+        Optional<Book> bookOP = Optional.of(book);
+        when(bookRepository.findById(id)).thenReturn(bookOP);
+
+        // when
+        BookRespDto bookRespDto = bookService.책수정하기(id,bookSaveReqDto);
+
+        // then
+
+        assertThat(bookRespDto.getTitle()).isEqualTo("스프링강의");
+        assertThat(bookRespDto.getAuthor()).isEqualTo("겟인데어");
+
     }
 }
