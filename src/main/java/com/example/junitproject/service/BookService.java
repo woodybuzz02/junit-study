35e17e6af4a76eae38b674a6d07bc001bc5e0116
yewon <ajdanddl6321@gmail.com>
2023-02-16 20:44:08 +0900

    책목록보기 서비스 완료

--- src/main/java/com/example/junitproject/service/BookService.java
+++ src/main/java/com/example/junitproject/service/BookService.java
@@ -8,6 +8,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 @RequiredArgsConstructor
 @Service
 public class BookService {
@@ -17,11 +21,19 @@ public class BookService {
     // 1. 책등록
     @Transactional(rollbackFor = RuntimeException.class) // RuntimeException발생하면 롤백 발생
     public BookRespDto 책등록하기(BookSaveReqDto dto){
-        Book bookps = bookRepository.save(dto.toEntity()); // 영속화된 객체
-        return new BookRespDto().toDto(bookps);
+        Book bookPs = bookRepository.save(dto.toEntity()); // 영속화된 객체
+        return new BookRespDto().toDto(bookPs);
     }
     
     // 2. 책목록보기
+    public List<BookRespDto> 책목록보기(){
+        List<Book> bookPsList = bookRepository.findAll(); //영속화된 북을 리턴함
+        List<BookRespDto> bookRespDtoList = new ArrayList<>();
+        for(Book b : bookPsList){
+            bookRespDtoList.add(new BookRespDto().toDto(b));
+        }
+        return bookRespDtoList;
+    }
 
     // 3. 책한건보기
 
