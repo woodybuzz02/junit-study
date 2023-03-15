0212fef1804e9b0baab265919b965dbce9cb3adb
yewon <ajdanddl6321@gmail.com>
2023-03-15 20:37:39 +0900

    컨트롤러 테스트 getBookList_test 완료

--- src/test/java/com/example/junitproject/web/BookApiControllerTest.java
+++ src/test/java/com/example/junitproject/web/BookApiControllerTest.java
@@ -1,16 +1,21 @@
 package com.example.junitproject.web;
 
+import com.example.junitproject.domain.Book;
+import com.example.junitproject.domain.BookRepository;
 import com.example.junitproject.service.BookService;
 import com.example.junitproject.web.dto.request.BookSaveReqDto;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jayway.jsonpath.DocumentContext;
 import com.jayway.jsonpath.JsonPath;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.*;
+import org.springframework.test.context.jdbc.Sql;
+
 import static org.assertj.core.api.Assertions.*;
 
 // 통합테스트
@@ -18,10 +23,27 @@ import static org.assertj.core.api.Assertions.*;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class BookApiControllerTest {
 
+    @Autowired // DI
+    private BookRepository bookRepository;
+
     @Autowired
     private TestRestTemplate rt;
     private static ObjectMapper om;
     private static HttpHeaders headers;
+
+    @BeforeEach
+    public void 데이터준비(){
+
+        String title = "junit5";
+        String author = "메타코딩";
+        Book book = Book.builder()
+                .title(title)
+                .author(author)
+                .build();
+
+        bookRepository.save(book);
+
+    }
 
     @BeforeAll
     public static void init(){
@@ -53,4 +75,20 @@ public class BookApiControllerTest {
         assertThat(author).isEqualTo("겟엔데어");
     }
 
+    @Sql("classpath:db/tableInit.sql")
+    @Test
+    public void getBookList_test(){
+        // given
+        // when
+        HttpEntity<String> request = new HttpEntity<>(null, headers);
+        ResponseEntity<String> response = rt.exchange("/api/v1/book", HttpMethod.GET, request, String.class);
+        // then
+        DocumentContext dc = JsonPath.parse(response.getBody());
+        Integer code = dc.read("$.code");
+        String title = dc.read("$.body.items[0].title");
+
+        assertThat(code).isEqualTo(1);
+        assertThat(title).isEqualTo("junit5");
+
+    }
 }
