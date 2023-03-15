1daeb17ce3f01236b57c8963d78d0b6064d8475c
yewon <ajdanddl6321@gmail.com>
2023-03-15 20:44:43 +0900

    컨트롤러 테스트 getBookOne_test 완료

--- src/test/java/com/example/junitproject/web/BookApiControllerTest.java
+++ src/test/java/com/example/junitproject/web/BookApiControllerTest.java
@@ -91,4 +91,21 @@ public class BookApiControllerTest {
         assertThat(title).isEqualTo("junit5");
 
     }
+
+    @Sql("classpath:db/tableInit.sql")
+    @Test
+    public void getBookOne_test(){
+        // given
+        Integer id = 1;
+        // when
+        HttpEntity<String> request = new HttpEntity<>(null, headers);
+        ResponseEntity<String> response = rt.exchange("/api/v1/book/"+id, HttpMethod.GET, request, String.class);
+        // then
+        DocumentContext dc = JsonPath.parse(response.getBody());
+        Integer code = dc.read("$.code");
+        String title = dc.read("$.body.title");
+
+        assertThat(code).isEqualTo(1);
+        assertThat(title).isEqualTo("junit5");
+    }
 }
